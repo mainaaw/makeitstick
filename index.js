2689ca14d93584d89d1523905a5c55370b9fad64
Anthony Maina <anthony.maina@savana.co.ke>
2016-09-27 13:24:19 -0500

    First Template addition

--- index.js
+++ index.js
@@ -40,151 +40,32 @@ controller.hears(['hello', 'hi'], ['direct_message', 'direct_mention'], function
     });
 })
 
-controller.hears(['compare','contrast', 'difference','opposed','list'], ['direct_message', 'direct_mention'], function (bot, message) {
- 
- var askTypeOfComparison = function(err, convo) {
-      convo.ask('Is the relationship a hierarchy or of equal parts?', function(response, convo) {
-        if (response.text == 'hierarchy' || response.text == 'Hierarchy') {
-        convo.say('Great.');
-        askSimplicity(response, convo);
-        convo.next();
-      } else {
-        convo.say('Great.');
-        askNatureOfEqualParts(response, convo);
-        convo.next();
-      }
-      });
-    };
-
-    var askNatureOfEqualParts = function(response, convo) {
-      convo.ask('What is the nature of the individual parts? (Separate, Overlapping or Network)', function(response, convo) {
-        if (response.text == 'separate' || response.text == 'Separate') {
-        convo.say('Understood.')
-        pickNatureOfSeparate(response, convo);
-        convo.next();
-      } else if (response.text == 'Overlapping' || response.text == 'overlapping') {
-        convo.say('Understood.');
-        displayOverlappingChart(response,convo);
-        convo.next();
-      } else {
-      convo.say('Understood');
-      displayNetworkedChart(response,convo);
-      convo.next();
-      }
-      });
-    }; 
-
-    var displayOverlappingChart = function(response, convo) {
-      convo.say('Here is a chart with overlapping components.');
-    };
-
-    var displayNetworkedChart = function(response, convo) {
-      convo.say('Here is a chart with a network of components.');
-    };
-
-
-    var pickNatureOfSeparate = function(response, convo) {
-
-      convo.ask('Does it contain a central idea or not? (yes or no)',[
-      {
-        pattern: bot.utterances.yes,
-        callback: function(response,convo) {
-          convo.say('Great! Here is a chart to best portray info with a central idea.');
-          convo.next();
- 
-        }
-      },
-      {
-        pattern: bot.utterances.no,
-        callback: function(response,convo) {
-          convo.say('Great! Here is a chart to best portray info composed of several independent ideas.');
-          convo.next();
-        }
-      }, 
-      {
-        default: true,
-        callback: function(response,convo) {
-          convo.repeat();
-          convo.next();
-        }
-      }
-      ]);
-    };
-
-
-    var askSimplicity = function(response, convo) {
-      convo.ask('What is the complexity of this hierarchy? (Simple or Complex)', function(response, convo) {
-        if (response.text == 'simple' || response.text == 'Simple') {
-        convo.say('Understood.')
-        displaySimpleHierarchy(response, convo);
-        convo.next();
-      } else {
-        convo.say('Understood.')
-        displayComplexHierarchy(response, convo);
-        convo.next();
-      }
-      });
-    };
-    var displaySimpleHierarchy = function(response, convo) {   
-      convo.say('Here is your simple hierarchy chart comparison.')
-    };
-
-    var displayComplexHierarchy = function(response, convo) {   
-      convo.say('Here is your complex hierarchy chart comparison..')
-    };
-
-    bot.startConversation(message, askTypeOfComparison);
-
-});
-
-controller.hears(['market share','segment', 'market','share', 'each'], ['direct_message', 'direct_mention'], function (bot, message) {
-  var attachments = [{
-    title: 'Marimekko or 100% Stacked Bar Graph',
-    text: 'You will require a 100% Stacked bar graph or a Marimekko graph to better convey this information.',
-    image_url: 'https://lh5.googleusercontent.com/13NK89olH44_y-AB3pG76WCQq_eNtSCJU1OGkzKkNVeROL_Yw5AxAqF1PRFtR4kkQVs3fZCadw2GqhM=w1157-h816',
-    unfurl_media:true,
-    color: '#EF84B6'
-  }]
-
-  bot.reply(message, {
-    attachments: attachments
-  }, function (err, resp) {0
-    console.log(err, resp)
-  })
-
-})
-
-
-
-controller.hears(['track','change', 'changed','rate','grow','growth','process'], ['direct_message', 'direct_mention'], function (bot, message) {
+controller.hears(['capture','brainstorming', 'brainstorm','Brainstorm'], ['direct_message', 'direct_mention'], function (bot, message) {
 var askType = function(err, convo) {
-      convo.ask('Would you like to show this in a cylical or non-cyclical manner?', function(response, convo) {
+      convo.ask('How many ideas are you working with?', function(response, convo) {
+        var ideasNum = parseInt(response.text, 10);
         
-        if (response.text == 'cyclical') {
-        askSteps(response, convo);
+        if (ideasNum < 6) {
+        showFewIdeasHoneyComb(response, convo);
         convo.next();
-      } else {
+      } else if (ideasNum > 5 && ideasNum < 13) {
         
-        showNonCyclical(response, convo);
+        showMediumNumIdeasHoneyComb(response, convo);
         convo.next();
         }
+         else {
+          showHighNumIdeasHoneyComb(response,convo);
+          convo.next();
+         }
       });
     };
-    var askSteps = function(response, convo) {
-      convo.ask('How many steps are there in this process?(max. 5)', function(response, convo) {
-        convo.say('Understood.')
-        displayCorrectChart(response, convo);
-        convo.next();
-      });
-    };
-    
 
-var showNonCyclical = function(response, convo) {showNonCyclical
+var showFewIdeasHoneyComb = function(response, convo) {
     var attachments = [{
-    fallback: 'Sample Non-Cyclical Process Chart',
-    title: 'NonCyclical Process Chart',
-    text: 'Here is the best chart to convey this information.',
-    image_url: 'https://thumb1.shutterstock.com/display_pic_with_logo/1083515/124682863/stock-vector-colorful-process-chart-module-vector-illustration-124682863.jpg',
+    fallback: 'Honeycomb Brainstorm',
+    title: 'Honeycomb Brainstorm',
+    text: 'Here is the best chart to capture brainstorming ideas.',
+    image_url: 'https://lh4.googleusercontent.com/2sQ2Kx19wYnwLA47KXVX_adcF4Zil8X0y8JZ4VTxVQQDCucAc2LkSPEwh1FLOKE4wakcWqf0Q-InP_A=w1157-h816',
     unfurl_media: true,
     color: '#FF0000'
   }]
@@ -194,32 +75,40 @@ var showNonCyclical = function(response, convo) {showNonCyclical
   }, function (err, resp) {0
     console.log(err, resp)
   })
-
   };
 
-  var endConvo = function(response,convo) {
-         convo.stop();
+  var showMediumNumIdeasHoneyComb = function(response, convo) {
+    var attachments = [{
+    fallback: 'Honeycomb Brainstorm',
+    title: 'Honeycomb Brainstorm',
+    text: 'Here is the best chart to capture brainstorming ideas.',
+    image_url: 'https://lh4.googleusercontent.com/nCD-bnDRkllysrSFk1TKDJ3pL61Ylq92Y9rI_K0qHQrrMtBQAv6zHBK_673hiDvdQ9atV9OROkJr7js=w1157-h816',
+    unfurl_media: true,
+    color: '#FF0000'
+  }]
+
+  bot.reply(message, {
+    attachments: attachments
+  }, function (err, resp) {0
+    console.log(err, resp)
+    })
   };
 
-  var displayCorrectChart = function(response, convo) {
-    var chartNum = parseInt(response.text, 10);
-    if(chartNum == 1) {
-      convo.say('You picked 1.');
+  var showHighNumIdeasHoneyComb = function(response, convo) {
+    var attachments = [{
+    fallback: 'Honeycomb Brainstorm',
+    title: 'Honeycomb Brainstorm',
+    text: 'Here is the best chart to capture brainstorming ideas.',
+    image_url: 'https://lh5.googleusercontent.com/SUoZvZtOrYt65mNAJORs-WxEEznE1J03bVb1cNW42lmGqH19RqmR-HehzWlVSkJ-Y9vDtzYEZQIrnSQ=w1157-h816',
+    unfurl_media: true,
+    color: '#FF0000'
+  }]
 
-    } else if(chartNum == 2) {
-      convo.say('You picked 2.');
-
-    } else if(chartNum == 3) {
-      convo.say('You picked 3.');
-
-    } else if(chartNum == 4) {
-
-      convo.say('You picked 4.');
-
-    } else if(chartNum == 5) {
-      convo.say('You picked 5.');
-    }
-
+  bot.reply(message, {
+    attachments: attachments
+  }, function (err, resp) {0
+    console.log(err, resp)
+    })
   };
 
  bot.startConversation(message, askType);
